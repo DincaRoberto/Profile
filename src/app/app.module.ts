7e9cf2bd0a2b16adbb8ab20059dd8431ee90cbcf
Roberto Dinca <roberto.dinca@key4talent.com>
2017-03-23 13:40:11 +0100

    Add routing as a separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
 import {MaterialModule} from '@angular/material';
-import {RouterModule, Routes} from '@angular/router';
 
 import 'hammerjs';
 
 import {FakeServerModule} from './fake-server/fake-server.module';
 import {RestModule} from './rest/rest.module';
-
-
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -18,25 +15,7 @@ import {RegisterComponent} from './register/register.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import {ProfileService} from "./profile.service";
-
-
-const appRoutes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'profile', component: ProfileComponent, data:{profile:"profileData"}},
-    //{path: 'hero/:id', component: HeroDetailComponent},
-    // {
-    //     path: 'heroes',
-    //     component: HeroListComponent,
-    //     data: {title: 'Heroes List'}
-    // },
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {path: '**', component: PageNotFoundComponent}
-];
+import { RoutingModule } from "./routing/routing.module";
 
 
 @NgModule({
@@ -55,7 +34,7 @@ const appRoutes: Routes = [
         MaterialModule,
         FakeServerModule,
         RestModule,
-        RouterModule.forRoot(appRoutes)
+        RoutingModule
     ],
     providers: [ProfileService],
     bootstrap: [AppComponent]
